93a77ee9d66e7eebd7671461e823406081bac6ba
dnyanu18 <dnyaneshwarbpatil45@gmail.com>
2025-11-20 10:57:05 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-// Select DOM elements
+// Seelect DOM elements
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
